efb45e5c808d4e1f8156112e19432f0a509b33e3
Clem <lem.chiew.choon@gmail.com>
2020-07-17 09:42:38 +0800

    Setting up manual acknowledgement message

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -9,15 +9,21 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connecting to NATS');
 
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group'
+    'orders-service-queue-group',
+    options
   );
+
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()} - with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
