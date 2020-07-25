7549158b3fa0dbf0d639d60d6b90f869fdb843e4
Clem <lem.chiew.choon@gmail.com>
2020-07-25 12:05:55 +0800

    Created UT for Orders_Created

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -42,7 +42,7 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs);
 };
 
-ticketSchema.statics.isReserved = async function () {
+ticketSchema.methods.isReserved = async function () {
   // this === the ticket document that we just called 'isReserved on
   const existingOrder = await Order.findOne({
     ticket: this,
